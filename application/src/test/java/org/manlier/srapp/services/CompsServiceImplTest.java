0635301adc994bcc29619716d666271052a3b355
pure glay <1490215053@qq.com>
2018-03-16 20:59:25 +0800

    add and modify configuration files

--- application/src/test/java/org/manlier/srapp/services/CompsServiceImplTest.java
+++ application/src/test/java/org/manlier/srapp/services/CompsServiceImplTest.java
@@ -4,29 +4,32 @@ import org.apache.solr.client.solrj.SolrServerException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.manlier.srapp.Application;
+import org.manlier.srapp.entities.Component;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.inject.Inject;
+
 import java.io.IOException;
 import java.util.List;
 
+import static org.junit.Assert.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
-public class SolrSearchingServiceTest {
+public class CompsServiceImplTest {
 
-	@Inject
-	private SolrSearchingService service;
+    @Inject
+    private CompsService service;
 
-	@Test
-	public void testInit() {
+    @Test
+    public void testInit() {
 
-	}
+    }
 
-	@Test
-	public void testSearching() throws IOException, SolrServerException {
-		List<String> ids = service.searchComps("45");
-		System.err.println(ids);
-	}
-
+    @Test
+    public void searchComps() throws IOException, SolrServerException {
+        List<Component> ids = service.searchComps("取消选取", 3);
+        System.err.println(ids);
+    }
 }
