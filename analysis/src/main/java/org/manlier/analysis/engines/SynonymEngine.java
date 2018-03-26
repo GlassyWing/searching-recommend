3abda063b66b166372e944385acffd92d2da432a
pure glay <1490215053@qq.com>
2018-03-26 16:27:22 +0800

    移除不必要的模块

--- analysis/src/main/java/org/manlier/analysis/engines/SynonymEngine.java
+++
@@ -1,9 +0,0 @@
-package org.manlier.analysis.engines;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface SynonymEngine {
-
-	List<String> getSynonyms(String s) throws IOException;
-}
