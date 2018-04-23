be2a980030dc8da5e9b42f010b126b7e38df285d
pure glay <1490215053@qq.com>
2018-04-23 18:00:56 +0800

    增加索引控制层

---
+++ application/src/main/java/org/manlier/srapp/controllers/RebuildIndexController.java
@@ -0,0 +1,22 @@
+package org.manlier.srapp.controllers;
+
+import org.manlier.srapp.index.RebuildIndexService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RebuildIndexController {
+
+    private RebuildIndexService service;
+
+    public RebuildIndexController(RebuildIndexService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/api/v1/reindex")
+    public ResponseEntity rebuildIndex() {
+        service.rebuildIndex();
+        return ResponseEntity.ok("success");
+    }
+}
