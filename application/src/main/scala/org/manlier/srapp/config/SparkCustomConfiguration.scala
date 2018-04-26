b02dc703c262e4dd6966186fbecdd7ec364f3127
pure glay <1490215053@qq.com>
2018-04-26 20:14:18 +0800

    移除无用的包导入声明

--- application/src/main/scala/org/manlier/srapp/config/SparkCustomConfiguration.scala
+++ application/src/main/scala/org/manlier/srapp/config/SparkCustomConfiguration.scala
@@ -1,7 +1,5 @@
 package org.manlier.srapp.config
 
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.springframework.context.annotation
